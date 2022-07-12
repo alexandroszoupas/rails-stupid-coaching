500af8db0ad19a743ac6a59784d09c478ff2de6b
Alexandros Zoupas <alexandros.p.zoupas@gmail.com>
2022-07-12 21:08:02 +0300

    coach is stupid

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
     if question == 'I am going to work'
       params[:answer] = 'Great'
     elsif params[:whatever].end_with?("?")
-      params[:answer] = "Silly question dude, get dressed and go to work!"
+      params[:answer] = "Silly question friend, get dressed and go to work!"
     else
       params[:answer] = "I don\'t care, get dressed and go to work!"
     end
